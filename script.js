79bd1a78003de8006c1dd716d266f9ae1fa14f27
jaketverberg <54115094+jaketverberg@users.noreply.github.com>
2021-09-13 12:42:44 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,3 @@
-//Set forth the choices of the game
-let posibilities = ["rock", "paper", "scissors"];
-
-//Variables keeping the score of the game
-let playerWins = 0,
-compWins = 0;
-
-//This will be how the computer makes its random choice later
-function computerPlay () {
-return posibilities[Math.floor(Math.random() * 3)];
-}
-
-
 let posibilities = ["rock", "paper", "scissors"];
 
 const scoreContainer = document.querySelector('Scores')
